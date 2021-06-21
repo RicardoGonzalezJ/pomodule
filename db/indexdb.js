3c2adbbcb0a9b7d7d33dd04bf2bcde176c2d809c
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2021-06-21 13:15:07 -0500

    Db connected

--- db/indexdb.js
+++ db/indexdb.js
@@ -1,4 +1,22 @@
 import { default as util } from 'util';
-import  Pool  from 'pg';
+import  { default as pg }  from 'pg';
+const { Pool } = pg;
 
-console.log(Pool);
+const pool = new Pool({
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+});
+
+(async () => {
+    const client = await pool.connect();
+    console.log(client);
+    client.release();
+})();
+
+export default pool;
