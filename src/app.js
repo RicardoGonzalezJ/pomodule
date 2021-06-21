b0d4275192862a00cfe7e6230dc00c28f8bb4c3a
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2021-06-21 16:14:19 -0500

    Add default error handler

--- src/app.js
+++ src/app.js
@@ -7,7 +7,18 @@ app.use(logger('dev'));
 app.use(express.json());
 
 app.get('/', (req, res, next) => {
-    res.end('<h1>Hola po module</h1>');
+    
+    res.end('<h1>Hello po module</h1>')
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 export default app;
 
