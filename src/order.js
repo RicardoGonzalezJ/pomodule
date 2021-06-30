3939cf8155c112b05034850527d9fd71ee015de6
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2021-06-30 15:53:05 -0500

    Add Delete function for order_detail

--- src/order.js
+++ src/order.js
@@ -1,4 +1,4 @@
-import dbcon from '../db/indexdb.js';
+import dbcn from '../db/indexdb.js';
 
 /****************************************
  ************ MODEL FOR ORDER ***********/
@@ -6,15 +6,54 @@ export async function saveNewOrder(order) {
     const query = `INSERT INTO porder (storeid, ordernumber, orderdate, supplierid,
                                         orderdelyvery, orderpayment, employeeid, ordertotal)
                         VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING storeid, ordernumber;`;
-    const values = [order.storeid, order.number, order.supplierid, order.delivery, order.payment,
+    const values = [order.storeid, order.number, order.date, order.supplierid, order.delivery, order.payment,
                     order.employeeid, order.total];
 
     let res;
     try {
-        res = await dbcon.query(query, values);
+        res = await dbcn.query(query, values);
         return res;
     } catch (error) {
         console.log('Error saveNewOrder: ', error);
+    }
+}
+
+export async function saveOrderDetails(details) {
+    const query = `INSERT INTO order_detail (storeid, ordernumber, itemid, 
+                                            itemqty, item_unit_price, itemsubtotal)
+                    VALUES ($1, $2, $3, $4, $5, $6);`;
+    const values = [details.storeid, details.ordernumber, details.itemid,
+                    details.qty, details.unit_price, (details.qty * details.unit_price)];
+    let res;
+    try {
+        res = await dbcn.query(query, values);
+    } catch (error) {
+        console.log('Error saveOrderDetails: ', error.stack);
+    }
+}
+
+export async function deleteOrder(storeid, ordernumber) {
+    const query = 'DELETE FROM porder WHERE storeid = $1 AND ordernumber = $2;';
+    const value = [storeid, ordernumber];
+    let res;
+    try {
+        res = await dbcn.query(query, value);
+        return res;
+    } catch (error) {
+        console.log('error deleteOrder: ', error.stack);
+    }
+    
+}
+
+export async function deleteOrderDetail(storeid, ordernumber) {
+    const query = 'DELETE FROM order_detail WHERE storeid = $1 AND ordernumber = $2;';
+    const value = [storeid, ordernumber];
+    let res;
+    try {
+        res = await dbcn.query(query, value);
+        return res;
+    } catch (error) {
+        console.log('deleteOrderDetail: ', error.stack);
     }
 }
 
