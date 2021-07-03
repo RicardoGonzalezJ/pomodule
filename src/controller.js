233b45ea9daac3bd79b5324250f7f6b7287231c6
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2021-07-03 00:08:55 -0500

    Fix Bug: Error: Timeout Error: Timeout of 2000ms exceeded. For async tests and hooks, ensure "done()" is called; if returning a Promise, ensure it resolves. add a try/catch to the entire code in the controller.js addNewOrder. At the for..in there was null props. Inserted nested if to avoid null props.

--- src/controller.js
+++ src/controller.js
@@ -19,8 +19,83 @@ export async function getOrderInfo(req, res) {
         const storeInfo = await selectAllStore();
         const supplierInfo = await selectAllSuppliers();
         const employeeInfo = await selectAllEmployee();
-        res.status(200).json({itemInfo, storeInfo, supplierInfo, employeeInfo})
+        // res.status(200).json({itemInfo, storeInfo, supplierInfo, employeeInfo})
+        res.send(`<html>
+                    <head>
+                     <title>Test</title>
+                    </head>
+                    <body>
+                     <h2>Create Order Test</h2>
+                     <form action="/test" method="POST">
+                        <h3>Order Data</h3>
+                            <input type="number" name="storeid" placeholder="storeid">
+                            <input type="number" name="onumber" placeholder="ordernumber">
+                            <input type="date" name="orderDate">
+                            <input type="number" name="supplierid" placeholder="supplier id">
+                            <input type="text" name="delivery" placeholder="delivery">
+                            <input type="text" name="payment" placeholder="payment">
+                            <input type="number" name="employeeid" placeholder="employee id">
+                        <hr/>
+                        <h3>Order Details</h3>
+                            <input type="number" name="orderD0[itemid]" placeholder="itemid">
+                            <input type="number" name="orderD0[qty]" placeholder="quantity">
+                            <input type="number" name="orderD0[unit_price]" placeholder="unit_price">
+                            <input type="number" name="orderD0[subtotal]" placeholder="subtotal">
+                            <br/>
+                            <br/>
+                            <input type="number" name="orderD1[itemid]" placeholder="itemid">
+                            <input type="number" name="orderD1[qty]" placeholder="quantity">
+                            <input type="number" name="orderD1[unit_price]" placeholder="unit_price">
+                            <input type="number" name="orderD1[subtotal]" placeholder="subtotal">
+                            <br/>
+                            <br/>
+                        <input type="submit" value="submit">
+                    </form>
+                    </body>
+                </html>`)
     } catch (error) {
         console.log('getOrderInfo: ', error.stack);
     }
+}
+
+export async function addNewOrder(req, res) {
+    try {
+        let orderDetails = [];
+        let subtotals = [];
+        
+        const { storeid, onumber, orderDate, supplierid, 
+                delivery, payment, employeeid, total } = req.body;
+
+        const order = {
+            storeid,
+            onumber,
+            orderDate,
+            supplierid,
+            delivery,
+            payment,
+            employeeid,
+            total
+        };
+        
+        for (const props in req.body) {
+            if (typeof req.body[props] === 'object') {
+                if (req.body[props] !== null) {
+                    req.body[props].storeid = order.storeid;
+                    req.body[props].onumber = order.onumber;
+                    req.body[props].subtotal = req.body[props].qty * req.body[props].unit_price;
+                    orderDetails.push(req.body[props]);
+                    subtotals.push(req.body[props].subtotal);
+                }
+            }      
+        }
+        order.total = await totalOfTheOrder(subtotals);
+        console.log('odetail ',orderDetails);
+        console.log('osubtotals ', subtotals, 'total', order.total);
+        // res.status(301).redirect('/getorderinfo');
+        res.status(200).send('Done');    
+    } catch (error) {
+        res.status(404).send(error.stack)
+        console.log('message from addNewOrder: ', error.stack);
+    }
+    
 }
--- tests/order.test.js
+++ tests/order.test.js
@@ -3,49 +3,54 @@ import chaiHttp from 'chai-http';
 const expect = Chai.expect;
 Chai.use(chaiHttp);
 import app  from '../src/app.js';
-import { displayItems } from '../src/item.js';
-import { saveNewOrder, saveOrderDetails, deleteOrder, deleteOrderDetail } from '../src/order.js';
-import { totalOfTheOrder } from '../src/helpers.js';
 
 
-describe('=====Testing order queries=====', () => {
+describe('=====Testing POST /test=====', () => {
 
-    let totalArray;
-
-    const order = {
-        storeid: 1,
-        number: 1,
-        date: '2021-06-29',
-        supplierid: 1,
-        delivery: 'by sea',
-        payment: 'CC',
-        employeeid: 2,
-        total: null
-    }
-
-    const orderDetail = [
-        {
-            storeid: 1,
-            ordernumber: 1,
-            itemid: 1,
-            qty: 3,
-            unit_price: 10.00,
-            subtotal: null
-         },
-         {
-            storeid: 1,
-            ordernumber: 1,
-            itemid: 2,
-            qty: 5,
-            unit_price: 10.00,
-            subtotal: null
-         }
-    ]
-
-    let subtotal1 = orderDetail[0].subtotal = orderDetail[0].qty * orderDetail[0].unit_price;
-    let subtotal2 = orderDetail[1].subtotal = orderDetail[1].qty * orderDetail[1].unit_price;
-
-    it('should insert order into porder', async () => {
+    it('Should redirect to getorderinfo', async () => {
+        
+        try {
+            let res = await Chai
+            .request(app)
+            .post('/test')
+            .set('Content-Type', 'application/json')
+            .send({ 
+                storeid: 1,
+                onumber: 1,
+                orderDate: '2021-06-29',
+                supplierid: 1,
+                delivery: 'by sea',
+                payment: 'CC',
+                employeeid: 2,
+                total: null,
+                item1: {
+                    itemid: 1,
+                    qty: 3,
+                    unit_price: 10.00,
+                    subtotal: null
+                },
+                item2: {
+                    itemid: 2,
+                    qty: 5,
+                    unit_price: 50.00,
+                    subtotal: null
+                },
+                item3: {
+                    itemid: 3,
+                    qty: 4,
+                    unit_price: 30.00,
+                    subtotal: null
+                } 
+        })
+        // console.log(res);
+        expect(res).to.have.status(200);
+          
+        } catch (error) {
+            console.log('error on test post test: ', error.stack);
+        }
+        
+    });
+    /*it('should insert order into porder', async () => {
         totalArray = [subtotal1, subtotal2];
         order.total = await totalOfTheOrder(totalArray);
         await saveNewOrder(order);
@@ -70,5 +75,5 @@ describe('=====Testing order queries=====', () => {
             await deleteOrderDetail(item.storeid, item.ordernumber);
         });
         await deleteOrder(order.storeid, order.number);
-    });
+    });*/
 });
